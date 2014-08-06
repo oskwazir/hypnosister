658206ef4bf53070f2f5cba30e1c8e5ac92aa777
Omer Wazir <omer.wazir@gmail.com>
2014-08-05 20:35:59 -0700

    Able to draw a triangle. It's not how it should be. gradient is using wrong colors still.

--- objective-c/Hypnosister/BNRHypnosisView.m
+++ objective-c/Hypnosister/BNRHypnosisView.m
@@ -66,8 +66,49 @@
     //Now draw the line
     [path stroke];
     
+
+    
     //get the current graphics context
     CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    
+    //save the context before applying a clipping path to it
+    CGContextSaveGState(currentContext);
+    
+    //All the work in making a clipping path
+    CGPoint startPoint = CGPointMake(bounds.origin.x + bounds.size.width / 2.0, bounds.origin.y + bounds.size.height * 0.20);
+    CGPoint endPoint = CGPointMake(bounds.origin.x + bounds.size.width, bounds.origin.y + bounds.size.height * 0.85);
+    
+    CGFloat angle = M_PI/4; // 45 degrees in radians
+    // v1 = vector from startPoint to endPoint:
+    CGPoint v1 = CGPointMake(endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+    // v2 = v1 rotated by 60 degrees:
+    CGPoint v2 = CGPointMake(cosf(angle) * v1.x - sinf(angle) * v1.y,
+                             sinf(angle) * v1.x + cosf(angle) * v1.y);
+    // thirdPoint = startPoint + v2:
+    CGPoint thirdPoint = CGPointMake(startPoint.x + v2.x, startPoint.y + v2.y);
+    
+    [path moveToPoint:startPoint];
+    [path addLineToPoint:endPoint];
+    [path addLineToPoint:thirdPoint];
+    
+    //now apply the clipping path
+    [path addClip];
+    
+    //apply the gradient which is applied just within the clipping path
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = { 1.0, 0.0, 0.0, 1.0, //some how this starts as red
+        1.0,1.0,0.0,1.0}; // and this ends as yellow?
+    
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace, components, locations, 2);
+    CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, 0);
+    
+    //Need to release what we created since own them
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(colorspace);
+    
+    //restore state so a gradient and clipping aren't applied
+    CGContextRestoreGState(currentContext);
     
     //save the context before applying a shadow to it
     CGContextSaveGState(currentContext);
@@ -77,6 +118,11 @@
     
     //Add hypnotoad
     [hypnotoad drawInRect:hypnotoadBounds];
+    
+    //restore the context so new drawings don't have a shadow applied to them.
+    CGContextRestoreGState(currentContext);
+    
+    
 }
 
 
