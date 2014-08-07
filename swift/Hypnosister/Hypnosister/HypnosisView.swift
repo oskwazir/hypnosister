3d2072654ad36c931fd800f8a191937363fd87e7
Omer Wazir <omer.wazir@gmail.com>
2014-08-07 09:03:42 -0700

    Draw concentric circles

--- swift/Hypnosister/Hypnosister/HypnosisView.swift
+++ swift/Hypnosister/Hypnosister/HypnosisView.swift
@@ -24,21 +24,28 @@ class HypnosisView: UIView {
         self.backgroundColor = UIColor.clearColor();
     }
     
-//     Only override drawRect: if you perform custom drawing.
-//     An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect)
     {
         let bounds:CGRect = self.bounds;
         let center:CGPoint = CGPoint(x: bounds.origin.x + bounds.size.width / 2.0,
             y: bounds.origin.y + bounds.size.height / 2.0);
-        let radius = min(bounds.size.width, bounds.size.height) / 2.0;
+        
+        //The largest circle will circumscribe the view
+        let maxRadius = hypot(bounds.size.height, bounds.size.width) / 2.0;
         let path:UIBezierPath = UIBezierPath();
         
         //Swift can't convert type Float to CGFloat...yet
         //So doing this kinda wonky workaround of creating startAngle and endAngle constants.
         let startAngle = CGFloat(0.0);
         let endAngle = CGFloat(M_PI * 2.0);
-        path.addArcWithCenter(center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true);
+        
+        for(var currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20){
+            path.moveToPoint(CGPointMake(center.x + currentRadius, center.y));
+            
+            path.addArcWithCenter(center, radius: currentRadius,
+                startAngle: startAngle, endAngle: endAngle, clockwise: true);
+        }
+        
         path.lineWidth = 10.0;
         UIColor.lightGrayColor().setStroke();
         path.stroke();
