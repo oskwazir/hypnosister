e590dd404576d5dcf760c158d5384185901ee032
Omer Wazir <omer.wazir@gmail.com>
2014-08-12 09:25:12 -0700

    Add gradient to clipping path behind Hypnotoad

--- swift/Hypnosister/Hypnosister/HypnosisView.swift
+++ swift/Hypnosister/Hypnosister/HypnosisView.swift
@@ -56,9 +56,42 @@ class HypnosisView: UIView {
         path.stroke();
         
         //get the current graphics context
-        let currentContext:CGContextRef = UIGraphicsGetCurrentContext();
+        var currentContext:CGContextRef = UIGraphicsGetCurrentContext();
         
         //save the context before applying a clipping path to it
+        CGContextSaveGState(currentContext);
+        
+        
+        //points for the clipping path
+        let startPoint:CGPoint = CGPointMake(bounds.origin.x + (bounds.size.width / 2.0), bounds.origin.y + (bounds.size.height * 0.15) );
+        let secondPoint:CGPoint = CGPointMake(bounds.origin.x + bounds.size.width - 20, bounds.origin.y + (bounds.size.height * 0.85) );
+        let thirdPoint:CGPoint = CGPointMake(bounds.origin.x + 20, bounds.origin.y + (bounds.size.height * 0.85) );
+        
+        //make a new path, but can I use the existing one?
+        let triangle:UIBezierPath = UIBezierPath();
+        triangle.moveToPoint(startPoint);
+        triangle.addLineToPoint(secondPoint);
+        triangle.addLineToPoint(thirdPoint);
+        triangle.closePath();
+        
+        //now apply the clipping path
+        triangle.addClip();
+        
+        //apply the gradient to the inside of the triangle
+        let locations:[CGFloat] = [0.0,1.0];
+        let components:[CGFloat] = [0.0, 1.0, 0.0, 1.0, 1.0,1.0,0.0,1.0];
+        let endPoint:CGPoint = CGPointMake(bounds.origin.x + (bounds.size.width / 2.0), bounds.origin.y + (bounds.size.height * 0.85) );
+        
+        let colorSpace:CGColorSpaceRef = CGColorSpaceCreateDeviceRGB();
+        let gradient:CGGradientRef = CGGradientCreateWithColorComponents(colorSpace, components, locations, 2);
+        
+        //swift doesn't like kCGGradientDrawsAfterEndLocation 
+        CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, 0);
+        
+        CGContextRestoreGState(currentContext);
+        
+        
+        //save the context before applying a shadow to it
         CGContextSaveGState(currentContext);
         
         //set a shadow to the context
