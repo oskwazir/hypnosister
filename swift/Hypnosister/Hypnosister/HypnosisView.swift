68f57401ed0e00d1f7351948876a8985245dc026
Omer Wazir <omer.wazir@gmail.com>
2014-08-12 20:48:03 -0700

    Circle change color on touch event

--- swift/Hypnosister/Hypnosister/HypnosisView.swift
+++ swift/Hypnosister/Hypnosister/HypnosisView.swift
@@ -15,22 +15,40 @@ class HypnosisView: UIView {
     **   “Class HypnosisView does not implement its superclass's required members”
     **   stackoverflow.com/questions/25126295/swift-class-does-not-implement-its-superclasss-required-members
     ***/
+    
+    var circleColor:UIColor {
+        
+        //does didSet get called in the init since it's the first time it's being set?
+        //or do I need to set the value in the declaration for it not to fire the first time?
+        didSet {
+            self.setNeedsDisplay();
+        }
+    };
+    
     required init(coder: NSCoder) {
         fatalError("NSCoding not supported")
     }
     
     override init(frame: CGRect) {
+        self.circleColor = UIColor.lightGrayColor();
         super.init(frame:frame);
         self.backgroundColor = UIColor.clearColor();
+        
+    }
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        print("\(self) was touched");
+        let red = CGFloat(arc4random() % 100) / 100.0;
+        let blue = CGFloat(arc4random() % 100) / 100.0;
+        let green = CGFloat(arc4random() % 100) / 100.0;
+        let randomColor:UIColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0);
+        
+        self.circleColor = randomColor;
     }
     
     override func drawRect(rect: CGRect)
     {
         let bounds:CGRect = self.bounds;
-        let hypnoToadBounds:CGRect = CGRectMake(bounds.origin.x + bounds.size.width / 4.0,
-            bounds.origin.y + bounds.size.height / 4.0, bounds.size.width / 1.58, bounds.size.height / 2.0);
-    
-        let hypnoToad:UIImage = UIImage(named: "hypnotoad.png");
         
         let center:CGPoint = CGPoint(x: bounds.origin.x + bounds.size.width / 2.0,
             y: bounds.origin.y + bounds.size.height / 2.0);
@@ -52,56 +70,9 @@ class HypnosisView: UIView {
         }
         
         path.lineWidth = 10.0;
-        UIColor.lightGrayColor().setStroke();
+        self.circleColor.setStroke();
         path.stroke();
         
-        //get the current graphics context
-        var currentContext:CGContextRef = UIGraphicsGetCurrentContext();
-        
-        //save the context before applying a clipping path to it
-        CGContextSaveGState(currentContext);
-        
-        
-        //points for the clipping path
-        let startPoint:CGPoint = CGPointMake(bounds.origin.x + (bounds.size.width / 2.0), bounds.origin.y + (bounds.size.height * 0.15) );
-        let secondPoint:CGPoint = CGPointMake(bounds.origin.x + bounds.size.width - 20, bounds.origin.y + (bounds.size.height * 0.85) );
-        let thirdPoint:CGPoint = CGPointMake(bounds.origin.x + 20, bounds.origin.y + (bounds.size.height * 0.85) );
-        
-        //make a new path, but can I use the existing one?
-        let triangle:UIBezierPath = UIBezierPath();
-        triangle.moveToPoint(startPoint);
-        triangle.addLineToPoint(secondPoint);
-        triangle.addLineToPoint(thirdPoint);
-        triangle.closePath();
-        
-        //now apply the clipping path
-        triangle.addClip();
-        
-        //apply the gradient to the inside of the triangle
-        let locations:[CGFloat] = [0.0,1.0];
-        let components:[CGFloat] = [0.0, 1.0, 0.0, 1.0, 1.0,1.0,0.0,1.0];
-        let endPoint:CGPoint = CGPointMake(bounds.origin.x + (bounds.size.width / 2.0), bounds.origin.y + (bounds.size.height * 0.85) );
-        
-        let colorSpace:CGColorSpaceRef = CGColorSpaceCreateDeviceRGB();
-        let gradient:CGGradientRef = CGGradientCreateWithColorComponents(colorSpace, components, locations, 2);
-        
-        //swift doesn't like kCGGradientDrawsAfterEndLocation 
-        CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, 0);
-        
-        CGContextRestoreGState(currentContext);
-        
-        
-        //save the context before applying a shadow to it
-        CGContextSaveGState(currentContext);
-        
-        //set a shadow to the context
-        CGContextSetShadow(currentContext, CGSizeMake(4, 6), 2.5);
-        
-        hypnoToad.drawInRect(hypnoToadBounds);
-        
-        //restore the context so new drawings don't have a  shadow on them
-        CGContextRestoreGState(currentContext);
-    
     }
 
 }
